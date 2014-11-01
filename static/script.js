d6075ab98984a792f0aef8e32c3541b20d5159c9
Raymond Zeng <raymondzeng@live.com>
2014-11-01 03:33:15 -0400

    moment.js to format intervals

--- static/script.js
+++ static/script.js
@@ -2,14 +2,16 @@ var clientId = '998729760425-p0qih54o248tcj7g1ocs4nkm4bjr43km.apps.googleusercon
 var apiKey = 'S4cSTinwCBCiNjfEbnKmoGSo';
 var scopes = 'https://www.googleapis.com/auth/calendar';
 
-// Use a button to handle authentication the first time.
 function handleClientLoad() {
     gapi.client.setApiKey(apiKey);
     window.setTimeout(checkAuth,1);
 }
 
 function checkAuth() {
-    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+    gapi.auth.authorize({
+        client_id: clientId, 
+        scope: scopes, 
+        immediate: true}, handleAuthResult);
 }
 
 function handleAuthResult(authResult) {
@@ -24,11 +26,14 @@ function handleAuthResult(authResult) {
 }
 
 function handleAuthClick(event) {
-    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+    gapi.auth.authorize({
+        client_id: clientId, 
+        scope: scopes, 
+        immediate: false}, handleAuthResult);
     return false;
 }
 
-// Load the API and make an API call.  Display the results on the screen.
+// Load the API and make an API call. 
 function makeApiCall() {
     var calListRequest = gapi.client.request({
         'path': '/calendar/v3/users/me/calendarList',
@@ -36,31 +41,107 @@ function makeApiCall() {
     
     calListRequest.then(function(resp) {
         var ids = resp.result.items;
+        var items = [];
         for (var i = 0; i < ids.length; i++) {
-            console.log(ids[i].id);
+            items.push({'id': ids[i].id});
         }
+        freeBusyReq(items);
     }, function(reason) {
         console.log('Error: ' + reason.result.error.message);
     });
     
+}
+
+function freeBusyReq(items) {
     var freeBusyRequest = gapi.client.request({
         'path': '/calendar/v3/freeBusy',
         'method': 'POST',
         'body': {
-            "timeMin": "2014-10-01T00:00:00+00:00",
-            "timeMax": "2014-11-30T00:00:00+00:00",
-            "items": [{
-                "id": "raymond.dot.zeng@gmail.com"
-            }]
+            "timeMin": "2014-10-31T00:00:00+00:00",
+            "timeMax": "2014-11-05T00:00:00+00:00",
+            "items": items
         }
     });
     
     freeBusyRequest.then(function(resp) {
-        console.log(resp.result);
-        $("#content").html(JSON.stringify(resp.result));
+        var cals = resp.result.calendars;
+        var keys = Object.keys(cals);
+        var intervals = [];
+        for (var i = 0; i < keys.length; i++) {
+            var busyArr = cals[keys[i]].busy;
+            for (var j = 0; j < busyArr.length; j++) {
+                busyArr[j].start = new Date(busyArr[j].start);
+                busyArr[j].end = new Date(busyArr[j].end);
+            }
+            intervals = intervals.concat(busyArr);
+        }
+        
+        intervals = mergeIntervals(intervals);
+        l(intervals);
+        intervalsToText(intervals, "dddd MMM. D, h:mma", "h:mma zz", " - ");
     }, function(reason) {
         console.log('Error: ' + reason.result.error.message);
     });
-    
+}
 
+/**
+   arr is an array of objects with properties 
+   'start' and 'end', both of which are Date objects
+ **/
+function mergeIntervals(arr) {
+    l(arr);
+    // sort by start time
+    arr.sort(function(a, b){
+        return a.start - b.start;
+    });
+    
+    l(arr);
+    // iterate through intervals and merge them
+    var stack = [];
+    for (var i = 0; i < arr.length; i++) {
+        // stack is empty or this interval doesn't overlap with the last
+        // so just push this interval
+        if (stack.length == 0 ||
+            !overlaps(stack[stack.length - 1], arr[i])) {
+            stack.push(arr[i]);
+        } 
+        // otherwise merge this interval with the last one and push the merged
+        else {
+            var merged_intv = merge(stack.pop(), arr[i]);
+            stack.push(merged_intv);
+        }
+    }
+    return stack;
+}
+
+/**
+   Returns if a and b overlap.
+   Assumes a starts before or same time as b
+**/
+function overlaps(a, b) {
+    return b.start <= a.end;
+}
+
+function merge(a, b) {
+    return {
+        start: a.start,
+        end: b.end
+    };
+}
+
+function l(thing) {
+    console.log(thing);
+}
+
+/**
+   Given an array of intervals {'start': ... , 'end': ... },
+   return text describing those intervals.
+**/
+function intervalsToText(intervals, startFormat, endFormat, glue) {
+    var s = "";
+    for (var i = 0; i < intervals.length; i++) {
+        s += moment(intervals[i].start).format(startFormat) 
+            + glue + moment(intervals[i].end).format(endFormat) + "\n";
+    }
+    console.log(s);
 }
